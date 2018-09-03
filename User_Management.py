a3bdab3cea8ac77f074c3766b5b9ab2f75ed49c3
Sun <xxsunxxrose@gmail.com>
2018-09-04 04:47:29 +0530

    created new file

---
+++ User_Management.py
@@ -0,0 +1,30 @@
+
+#import sunlite
+
+from sunlite.db import connect
+
+db = connect("user")
+
+
+def add_user(name,age,phone,mail):
+    #create a header with the name
+    db.new(name)
+    db.push("age",age)
+    db.push("phone",phone)
+    db.push("mail",mail)
+    return True
+
+def find_all_data(name):
+    return db.pull(name)
+
+def find_specific_data(data):
+    return db.pull(data)
+
+def update_data(name,data):
+    return db.push(name.data)
+
+print("Enter Command :")
+while True:
+    o = input(">> ")
+    print(eval(o))
+
